f70f6e4321251fb4e61c13c578fbf73df31d035b
hanneslindberg <hannes.lindberg@elev.ga.ntig.se>
2024-11-04 10:18:42 +0100

    hela koden fuckad för försökte dela upp rwquest i metoder

--- lib/request.rb
+++ lib/request.rb
@@ -1,36 +1,54 @@
+# frozen_string_literal: true
+
 class Request
   attr_reader :method, :resource, :headers, :version, :params, :path
 
   def initialize(request_string)
     header_section, body_section = request_string.split("\n\n", 2)
     lines = header_section.split("\n")
-    
+
     @headers = {}
     @params = {}
+    parse_first_line(line)
 
-    lines.map { |line| 
-      if line == lines[0]
-        @method, @resource, @version = line.split(" ")
-        @method = @method.downcase.to_sym
-        @path = @resource.split("?")[0]
-        query_string = @resource.split("?")[1]
+    if query_string
+      parse_params(params_string, query_string)
+    end
+  end
 
-        if query_string
-          params_string = query_string.split("&")
-          params_string.map { |param| 
-            key, value = param.split("=")
-            @params[key] = value
-          }
-        end
-      else
-        @headers[line.split(": ")[0]] = line.split(": ")[1]
-      end
-      if body_section
-        body_section.split("&").map { |param|
-        key, value = param.split("=")
+    def parse_first_line(line)
+      @method, @resource, @version = line.split(' ')
+      @method = @method.downcase.to_sym
+      return @method, @resource, @version
+    end
+
+    def parse_params(params_string, query_string)
+      params_string = query_string.split('&')
+      params_string.map do |param|
+        key, value = param.split('=')
         @params[key] = value
-        }
       end
-    }
+    end
+
+    lines.map do |line|
+      if line == lines[0]
+        
+        @path = @resource.split('?')[0]
+        query_string = @resource.split('?')[1]
+
+      else
+        @headers[line.split(': ')[0]] = line.split(': ')[1]
+      end
+
+      next unless body_section
+
+      def post_params(body_section)
+        body_section.split('&').map do |param|
+          key, value = param.split('=')
+          @params[key] = value
+        end
+      end
+
+    end
   end
 end
--- test/http_parser_spec.rb
+++ test/http_parser_spec.rb
@@ -1,6 +1,78 @@
 require_relative 'spec_helper'
 require_relative '../lib/request'
 require_relative '../lib/router'
+require 'simplecov'
+SimpleCov.start
+
+describe 'Request' do
+
+    describe 'Simple get-request' do
+    
+        it 'parses the http method' do
+            request_string = File.read('test/example_requests/get-index.request.txt')
+            request = Request.new(request_string)
+            _(request.method).must_equal :get
+        end
+
+        it 'parses the http method' do
+            request_string = File.read('test/example_requests/post-login.request.txt')
+            request = Request.new(request_string)
+            _(request.method).must_equal :post
+        end
+
+        it 'parses the resource' do
+            request_string = File.read('test/example_requests/get-index.request.txt')
+            request = Request.new(request_string)
+            _(request.resource).must_equal "/"
+        end
+
+        it 'parses the resource' do
+            request_string = File.read('test/example_requests/get-fruits-with-filter.request.txt')
+            request = Request.new(request_string)
+            _(request.resource).must_equal "/fruits?type=bananas&minrating=4"
+        end
+
+        it 'parses the resource' do
+            request_string = File.read('test/example_requests/get-examples.request.txt')
+            request = Request.new(request_string)
+            _(request.resource).must_equal "/examples"
+        end
+
+        it 'parses the version' do
+            request_string = File.read('test/example_requests/get-index.request.txt')
+            request = Request.new(request_string)
+            _(request.version).must_equal "HTTP/1.1"
+        end
+
+        it 'parses the headers' do
+            request_string = File.read('test/example_requests/get-index.request.txt')
+            request = Request.new(request_string)
+            headers = {"Host" => "developer.mozilla.org", "Accept-Language" => "fr"}
+            _(request.headers).must_equal headers
+        end
+
+        it 'parses the headers' do
+            request_string = File.read('test/example_requests/get-fruits-with-filter.request.txt')
+            request = Request.new(request_string)
+            headers = {"Host" => "fruits.com", "User-Agent" => "ExampleBrowser/1.0", "Accept-Encoding" => "gzip, deflate", "Accept" => "*/*"}
+            _(request.headers).must_equal headers
+        end
+
+        it 'parses the params for post' do
+            request_string = File.read('test/example_requests/post-login.request.txt')
+            request = Request.new(request_string)
+            params = {"username" => "grillkorv", "password" => "verys3cret!"}
+            _(request.params).must_equal params
+        end
+
+        it 'parses the params for get' do
+            request_string = File.read('test/example_requests/get-fruits-with-filter.request.txt')
+            request = Request.new(request_string)
+            params = {"type" => "bananas", "minrating" => "4"}
+            _(request.params).must_equal params
+        end
+    end
+end
 
 describe 'Router' do
 	let(:router) { Router.new }
